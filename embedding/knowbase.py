f6b28a1c41784ceb364be1eb36f58f348d3c22a8
taoyuan <1782663935@qq.com>
2023-06-18 17:36:13 +0800

    Add files via upload

---
+++ embedding/knowbase.py
@@ -0,0 +1,189 @@
+import datetime
+from functools import lru_cache
+from typing import List
+
+from langchain.docstore.document import Document
+from langchain.document_loaders import UnstructuredFileLoader, TextLoader
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from pypinyin import lazy_pinyin
+from tqdm import tqdm
+
+from config import *
+from torch_gc import torch_gc
+from myFAISS import MyFAISS
+from text_splitter import ChineseTextSplitter
+
+
+def _embeddings_hash(self):
+    return hash(self.model_name)
+
+
+HuggingFaceEmbeddings.__hash__ = _embeddings_hash
+
+
+@lru_cache(CACHED_VS_NUM)
+def load_vector_store(vs_path, embeddings):
+    return MyFAISS.load_local(vs_path, embeddings)
+
+
+def tree(filepath, ignore_dir_names=None, ignore_file_names=None):
+    """返回两个列表，第一个列表为 filepath 下全部文件的完整路径, 第二个为对应的文件名"""
+    if ignore_dir_names is None:
+        ignore_dir_names = []
+    if ignore_file_names is None:
+        ignore_file_names = []
+    ret_list = []
+    if isinstance(filepath, str):
+        if not os.path.exists(filepath):
+            print("路径不存在")
+            return None, None
+        elif os.path.isfile(filepath) and os.path.basename(filepath) not in ignore_file_names:
+            return [filepath], [os.path.basename(filepath)]
+        elif os.path.isdir(filepath) and os.path.basename(filepath) not in ignore_dir_names:
+            for file in os.listdir(filepath):
+                fullfilepath = os.path.join(filepath, file)
+                if os.path.isfile(fullfilepath) and os.path.basename(fullfilepath) not in ignore_file_names:
+                    ret_list.append(fullfilepath)
+                if os.path.isdir(fullfilepath) and os.path.basename(fullfilepath) not in ignore_dir_names:
+                    ret_list.extend(tree(fullfilepath, ignore_dir_names, ignore_file_names)[0])
+    return ret_list, [os.path.basename(p) for p in ret_list]
+
+
+def load_file(filepath, sentence_size=SENTENCE_SIZE):
+    if filepath.lower().endswith(".txt"):
+        loader = TextLoader(filepath, autodetect_encoding=True)
+        textsplitter = ChineseTextSplitter(pdf=False, sentence_size=sentence_size)
+        docs = loader.load_and_split(textsplitter)
+    else:
+        loader = UnstructuredFileLoader(filepath, mode="elements")
+        textsplitter = ChineseTextSplitter(pdf=False, sentence_size=sentence_size)
+        docs = loader.load_and_split(text_splitter=textsplitter)
+    write_check_file(filepath, docs)
+    return docs
+
+
+def write_check_file(filepath, docs):
+    folder_path = os.path.join(os.path.dirname(filepath), "tmp_files")
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    fp = os.path.join(folder_path, 'load_file.txt')
+    with open(fp, 'a+', encoding='utf-8') as fout:
+        fout.write("filepath=%s,len=%s" % (filepath, len(docs)))
+        fout.write('\n')
+        for i in docs:
+            fout.write(str(i))
+            fout.write('\n')
+        fout.close()
+
+
+class KnowledgeBase:
+    embeddings: object = None
+
+    def init_cfg(self,
+                 embedding_model: str = EMBEDDING_MODEL,
+                 embedding_device=EMBEDDING_DEVICE,
+                 ):
+        self.embeddings = HuggingFaceEmbeddings(model_name=embedding_model_dict[embedding_model],
+                                                model_kwargs={'device': embedding_device})
+
+    def init_knowledge_vector_store(self,
+                                    filepath: str or List[str],
+                                    vs_path: str or os.PathLike = None,
+                                    sentence_size=SENTENCE_SIZE):
+        loaded_files = []
+        failed_files = []
+        if isinstance(filepath, str):
+            if not os.path.exists(filepath):
+                print("路径不存在")
+                return None
+            elif os.path.isfile(filepath):
+                file = os.path.split(filepath)[-1]
+                try:
+                    docs = load_file(filepath, sentence_size)
+                    logger.info(f"{file} 已成功加载")
+                    loaded_files.append(filepath)
+                except Exception as e:
+                    logger.error(e)
+                    logger.info(f"{file} 未能成功加载")
+                    return None
+            elif os.path.isdir(filepath):
+                docs = []
+                for fullfilepath, file in tqdm(zip(*tree(filepath, ignore_dir_names=['tmp_files'])), desc="加载文件"):
+                    try:
+                        docs += load_file(fullfilepath, sentence_size)
+                        loaded_files.append(fullfilepath)
+                    except Exception as e:
+                        logger.error(e)
+                        failed_files.append(file)
+
+                if len(failed_files) > 0:
+                    logger.info("以下文件未能成功加载：")
+                    for file in failed_files:
+                        logger.info(f"{file}\n")
+        else:
+            docs = []
+            for file in filepath:
+                try:
+                    docs += load_file(file)
+                    logger.info(f"{file} 已成功加载")
+                    loaded_files.append(file)
+                except Exception as e:
+                    logger.error(e)
+                    logger.info(f"{file} 未能成功加载")
+        if len(docs) > 0:
+            logger.info("文件加载完毕，正在生成向量库")
+            if vs_path and os.path.isdir(vs_path) and "index.faiss" in os.listdir(vs_path):
+                vector_store = load_vector_store(vs_path, self.embeddings)
+                vector_store.add_documents(docs)
+                torch_gc()
+            else:
+                if not vs_path:
+                    vs_path = os.path.join(KB_ROOT_PATH,
+                                           f"""{"".join(lazy_pinyin(os.path.splitext(file)[0]))}_FAISS_{datetime.datetime.now().strftime("%Y%m%d_%H%M%S")}""",
+                                           "vector_store")
+                vector_store = MyFAISS.from_documents(docs, self.embeddings)  # docs 为Document列表
+                torch_gc()
+
+            vector_store.save_local(vs_path)
+            return vs_path, loaded_files
+        else:
+            logger.info("文件均未成功加载，请检查依赖包或替换为其他文件再次上传。")
+            return None, loaded_files
+
+    def one_knowledge_add(self, vs_path, one_title, one_conent, one_content_segmentation, sentence_size):
+        try:
+            if not vs_path or not one_title or not one_conent:
+                logger.info("知识库添加错误，请确认知识库名字、标题、内容是否正确！")
+                return None, [one_title]
+            docs = [Document(page_content=one_conent + "\n", metadata={"source": one_title})]
+            if not one_content_segmentation:
+                text_splitter = ChineseTextSplitter(pdf=False, sentence_size=sentence_size)
+                docs = text_splitter.split_documents(docs)
+            if os.path.isdir(vs_path) and os.path.isfile(vs_path + "/index.faiss"):
+                vector_store = load_vector_store(vs_path, self.embeddings)
+                vector_store.add_documents(docs)
+            else:
+                vector_store = MyFAISS.from_documents(docs, self.embeddings)  ##docs 为Document列表
+            torch_gc()
+            vector_store.save_local(vs_path)
+            return vs_path, [one_title]
+        except Exception as e:
+            logger.error(e)
+            return None, [one_title]
+
+    def delete_file_from_vector_store(self,
+                                      filepath: str or List[str],
+                                      vs_path):
+        vector_store = load_vector_store(vs_path, self.embeddings)
+        status = vector_store.delete_doc(filepath)
+        return status
+
+    def list_file_from_vector_store(self,
+                                    vs_path,
+                                    fullpath=False):
+        vector_store = load_vector_store(vs_path, self.embeddings)
+        docs = vector_store.list_docs()
+        if fullpath:
+            return docs
+        else:
+            return [os.path.split(doc)[-1] for doc in docs]
